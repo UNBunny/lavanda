933ec038783fca8da4ddb5a52351aebbca3e29ad
UNBunny <nikitakuharchuk07@gmail.com>
2025-09-16 23:33:57 +0600

    Initial  commit

---
+++ src/main/java/com/omstu/lavanda/LavandaApplication.java
@@ -0,0 +1,13 @@
+package com.omstu.lavanda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LavandaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LavandaApplication.class, args);
+    }
+
+}
