0388f0a857a89313ae56e069a73a402bee358ee5
Nikita Kuharchuk <112119548+UNBunny@users.noreply.github.com>
2025-09-17 00:27:57 +0600

    Revert "feat: set up basic structure"

--- src/main/java/com/omstu/lavanda/LavandaApplication.java
+++ src/main/java/com/omstu/lavanda/LavandaApplication.java
@@ -1,13 +1,13 @@
-package com.omstu.inventoryservice;
+package com.omstu.lavanda;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class InventoryServiceApplication {
+public class LavandaApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(InventoryServiceApplication.class, args);
+        SpringApplication.run(LavandaApplication.class, args);
     }
 
 }
---
+++ src/test/java/com/omstu/lavanda/LavandaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.omstu.lavanda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LavandaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
